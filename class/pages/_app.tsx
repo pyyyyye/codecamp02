75d4155c0d409389d4437327a68f79eda5c12e01
pyyyyye <qkzud8033@hanmail.net>
2021-07-16 17:09:22 +0900

    Update

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -1,17 +1,19 @@
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
-import { AppProps } from 'next/dist/next-server/lib/router/router'
-import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
-    uri:'http://backend02.codebootcamp.co.kr/graphql',
-    cache: new InMemoryCache()
-  })
+    uri: "http://backend02.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const aaa = "";
 
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
