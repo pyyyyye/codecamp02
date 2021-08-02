5fed88aff05a5dfeabbc92f228f3bdf1c6f88bf7
pyyyyye <qkzud8033@hanmail.net>
2021-08-02 22:43:35 +0900

    0802-2

--- freeboard_frontend/src/components/units/market/MarketLogin.presenter.tsx
+++ freeboard_frontend/src/components/units/market/MarketLogin.presenter.tsx
@@ -1,10 +1,44 @@
 // ---------- 중고마켓 로그인 presenter.tsx -------------
-import { Wrapper, Title } from './MarketLogin.styles';
+import { MiddleContentsPost } from '../product/DetailPage.styles';
+import {
+  Wrapper,
+  LoginWrap,
+  LoginContents,
+  TopLogo,
+  MiddleLoginBox,
+  InputLogin,
+  KeepStateLogin,
+  LoginCheckIcon,
+  KeepLoginText,
+  GoToLogin,
+  BottomJoinOrFind,
+  BottomBtn,
+} from './MarketLogin.styles';
 
 export default function MarketLoginUI() {
   return (
     <Wrapper>
-      <Title>마켓 로그인 페이지 입니다.</Title>
+      <LoginWrap>
+        <LoginContents>
+          <TopLogo />
+          <MiddleLoginBox>
+            <InputLogin value="text" placeholder="아이디를 입력해주세요" />
+            <InputLogin
+              value="password"
+              placeholder="비밀번호를 입력해주세요"
+            />
+            <KeepStateLogin>
+              <LoginCheckIcon>흥</LoginCheckIcon>
+              <KeepLoginText>로그인 상태 유지</KeepLoginText>
+            </KeepStateLogin>
+            <GoToLogin>로그인하기</GoToLogin>
+          </MiddleLoginBox>
+          <BottomJoinOrFind>
+            <BottomBtn>이메일 찾기</BottomBtn>|
+            <BottomBtn>비밀번호 찾기</BottomBtn>|<BottomBtn>회원가입</BottomBtn>
+          </BottomJoinOrFind>
+        </LoginContents>
+      </LoginWrap>
     </Wrapper>
   );
 }
--- freeboard_frontend/src/components/units/market/MarketLogin.styles.ts
+++ freeboard_frontend/src/components/units/market/MarketLogin.styles.ts
@@ -3,9 +3,72 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   width: 100%;
-  background-color: skyblue;
+  height: 100vh;
+  background-image: url('/market/market_bg.png');
 `;
-export const Title = styled.h1`
+export const LoginWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.8);
+  position: relative;
+`;
+export const LoginContents = styled.div`
+  width: 384px;
+  height: 510px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border: 5px solid blue;
+  display: flex;
+  flex-direction: column;
+`;
+export const TopLogo = styled.div`
+  width: 288px;
+  height: 44px;
+  border: 1px solid red;
+  margin: 0 auto;
+  background-image: url('/market/logo.png');
+`;
+export const MiddleLoginBox = styled.div`
+  width: 100%;
+  /* height: 64px;
+  border: 1px solid #fff;
+  border-radius: 16px; */
+  background-color: lemonchiffon;
+`;
+export const InputLogin = styled.input``;
+export const EmailInput = styled.div``;
+export const PasswordInput = styled.div``;
+export const KeepStateLogin = styled.div`
+  background-color: green;
+`;
+export const LoginCheckIcon = styled.div`
+  color: #fff;
+`;
+export const KeepLoginText = styled.div`
+  color: #fff;
+`;
+export const GoToLogin = styled.div`
+  height: 64px;
+  background-color: #4f4f4f;
+  border-radius: 16px;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 64px;
   text-align: center;
-  font-size: 50px; ;
+`;
+export const BottomJoinOrFind = styled.div`
+  background-color: brown;
+  display: flex;
+  justify-content: center;
+  color: #fff;
+`;
+export const BottomBtn = styled.div`
+  color: #fff;
+  font-weight: 400;
+  font-size: 14px;
+  padding: 0 20px;
+  cursor: pointer;
 `;
